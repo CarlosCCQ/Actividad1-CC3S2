84a9e745d2c32a5e7153b8ec467d24e148ef5830
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-08-27 23:45:01 -0500

    Corrección de app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.get('/',(req,res) => { //Define la ruta GET en la raiz ('/') que responde co
 
 const port = process.env.PORT || 3000; //Define el puerto en el que el servidor escuchara las solicitudes
 app.listen(port,() => { //Inicia el servidor Express en el puerto definido. Cuando el servidor comienza a escuchar, ejecuta la funcion callback
-    console.log('Server running on port ${port}'); //Imprime en la consola un mensaje indicando que puerto esta corriendo el servidor
+    console.log(`Server running on port ${port}`); //Imprime en la consola un mensaje indicando que puerto esta corriendo el servidor
 });
 
 module.exports = app; //Exporta la aplicacion 'app' para que pueda ser utilizada en otros modulos de Node.js
